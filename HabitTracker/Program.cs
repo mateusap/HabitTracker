d8ce063a5b3c23d1bd5729e57c855e8dacccd9e9
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-10-24 11:15:32 -0300

    Criada coneção com MySQL e inicio do menu

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MySql.Data.MySqlClient;
+
+string connectString = "server=localhost;userid=root;password=;database=habittracker";
+using (var connection = new MySqlConnection(connectString))
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+    tableCmd.CommandText =
+        @"CREATE TABLE IF NOT EXISTS agua (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        date TEXT,
+        quantity INTEGER)";
+    tableCmd.ExecuteNonQuery();
+    connection.Close();
+}
+
+static void GetInput()
+{
+    Console.Clear();
+    bool close = false;
+    while (close == false)
+    {
+        Console.WriteLine("\n\nMENU PRINCIPAL");
+        Console.WriteLine("\nO que você gostaria de fazer?");
+        Console.WriteLine("\nDigite 0 para encerrar o aplicativo");
+        Console.WriteLine("Digite 1 para ver os dados salvos");
+        Console.WriteLine("Digite 2 para inserir novos dados");
+        Console.WriteLine("Digite 3 para apagar dados");
+        Console.WriteLine("Digite 4 para atualizar dados");
+        Console.WriteLine("-----------------------------------------\n");
+
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "0":
+                Console.WriteLine("\nTchau!\n");
+                close = true;
+                break;
+            case "1":
+                GetAllRecords();
+                break;
+            case "2":
+                Insert();
+                break;
+            case "3":
+                Delete();
+                break;
+            case "4":
+                Update();
+                break;
+            default:
+                Console.WriteLine("\nComando inválido, digite um número entre 0 e 4.\n");
+                break;
+        }
+    }
+}
